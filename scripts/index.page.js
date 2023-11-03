b90e173c3d1014af7142ee2a12320578250f2a0d
Wanessa Lopes <wanessaalopes@gmail.com>
2023-11-03 15:47:49 -0600

    Set cursor to point on selected elements and font-family on input area

--- scripts/index.page.js
+++ scripts/index.page.js
@@ -1,21 +1,3 @@
-// const comments = [
-//   {
-//     name: "Connor Walton",
-//     timestamp: "02/17/2021",
-//     text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//   },
-//   {
-//     name: "Emilie Beach",
-//     timestamp: "01/09/2021",
-//     text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//   },
-//   {
-//     name: "Miles Acosta",
-//     timestamp: "12/20/2020",
-//     text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//   },
-// ];
-
 function displayComment(comment) {
   const commentsDiv = document.getElementById("comments");
 
@@ -79,6 +61,10 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const el = document.getElementById("myForm");
   if (el) {
     el.addEventListener("submit", functSubmit);
+  }
+  const comments = document.getElementById("comments");
+  if (comments) {
+    retrieveAndDisplayComments();
   }
 });
 
